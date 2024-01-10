05c2ce6e26953b1ed0a359b00f68035bba420d52
navneet195 <navneetbaldha195@gmail.com>
2024-01-10 12:41:03 +0530

    first commit

---
+++ NBOnboardingEcommerceApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NBOnboardingEcommerceApp
+//
+//  Created by Navnit Baldha on 10/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NBOnboardingEcommerceApp/NBOnboardingEcommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBOnboardingEcommerceAppApp.swift
+//  NBOnboardingEcommerceApp
+//
+//  Created by Navnit Baldha on 10/01/24.
+//
+
+import SwiftUI
+
+@main
+struct NBOnboardingEcommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
